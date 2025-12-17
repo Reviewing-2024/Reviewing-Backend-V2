1e216a4e549d0ef823be38f46e1e858905c42c29
seoyeoning <minalunpark@naver.com>
2025-12-17 20:38:27 +0900

    project init

---
+++ src/main/kotlin/my/reviewing/reviewing_V2/ReviewingV2Application.kt
@@ -0,0 +1,11 @@
+package my.reviewing.reviewing_V2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReviewingV2Application
+
+fun main(args: Array<String>) {
+	runApplication<ReviewingV2Application>(*args)
+}
---
+++ src/test/kotlin/my/reviewing/reviewing_V2/ReviewingV2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package my.reviewing.reviewing_V2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReviewingV2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
